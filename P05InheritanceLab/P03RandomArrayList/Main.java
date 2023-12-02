71bca3a3acc55000ddda169c7f8b3b012a76156b
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2023-12-02 03:49:58 +0200

    update

---
+++ P05InheritanceLab/P03RandomArrayList/Main.java
@@ -0,0 +1,13 @@
+package P05InheritanceLab.P03RandomArrayList;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        RandomArrayList random = new RandomArrayList();
+        random.getRandomElement();
+
+    }
+}
