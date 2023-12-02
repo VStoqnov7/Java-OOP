d27e092cbd43f35c2c63b27ba83caebc94d9c3ac
Vstoqnov7 <Ventsislav_stoyanov1996@abv.bg>
2023-12-02 03:50:21 +0200

    update

---
+++ P05InheritanceLab/P04StackOfStrings/StackOfStrings.java
@@ -0,0 +1,28 @@
+package P05InheritanceLab.P04StackOfStrings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StackOfStrings {
+
+    private List<String> data;
+
+    public StackOfStrings() {
+        this.data = new ArrayList<>();
+    }
+
+    public void push(String item){
+        data.add(item);
+    }
+    public String pop(){
+        String element = data.get(data.size() - 1);
+        data.remove(element);
+        return element;
+    }
+    public String peek(){
+        return data.get(data.size() - 1);
+    }
+    public boolean isEmpty(){
+        return data.isEmpty();
+    }
+}
